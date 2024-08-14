60d8541294afe38f13e24470dab0b653224a0f3f
yoksamutr <164991929+yoksamutr@users.noreply.github.com>
2024-08-14 21:30:01 +0700

    Create 02_​StrList_​F02.py

---
+++ 02/02_​StrList_​F02.py
@@ -0,0 +1,8 @@
+""""arabic numerals (function)"""
+
+def number_name(n):
+    num=int(n)
+    lt=["zero","one","two","three","four","five","six","seven","eight","nine"]
+    return lt[num]
+
+exec(input())
