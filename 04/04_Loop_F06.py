52437f2248fb6e54d3342a8acc1fe4244e3d6dbf
yoksamutr <164991929+yoksamutr@users.noreply.github.com>
2024-08-15 23:11:43 +0700

    Create 04_Loop_F06.py

---
+++ 04/04_Loop_F06.py
@@ -0,0 +1,15 @@
+"""print triangle (function)"""
+
+def print_triangle(h):
+    base=2*h-1
+    for i in range(1,h+1):
+        if i==1:
+            print("."*(h-1)+"*")
+        elif i==h:
+            print("*"*base)
+        else:
+            line="."*(h+i-2)+"*"
+            temp=line[0:h-i]+"*"+line[h-i+1:]
+            print(temp)
+        
+exec(input())
