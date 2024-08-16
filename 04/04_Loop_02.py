f93f7742a9b2e4949c3e970f040a2410b1b42c0d
yoksamutr <164991929+yoksamutr@users.noreply.github.com>
2024-08-16 10:46:12 +0700

    Create 04_Loop_02.py

---
+++ 04/04_Loop_02.py
@@ -0,0 +1,16 @@
+"""bisection log10"""
+
+import math
+
+a=float(input())
+start=0; end=a
+x=(start+end)/2
+
+while abs(a-(10**x))>(10**(-10))*max(a,10**x):
+    if 10**x>a:
+        end=x
+    elif 10**x<a:
+        start=x
+    x=(start+end)/2
+
+print(round(x,6))
