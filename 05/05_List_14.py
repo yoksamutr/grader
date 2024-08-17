734585507d34e39021bfef118f497dfdf410b58a
yoksamutr <164991929+yoksamutr@users.noreply.github.com>
2024-08-17 23:09:48 +0700

    Update 05_List_14.py

--- 05/05_List_14.py
+++ 05/05_List_14.py
@@ -6,8 +6,8 @@ count=0
 for i in range(1,len(y)-1):
     before=y[i-1]
     cur=y[i]
-    next=y[i+1]
-    if cur>before and cur>next:
+    after=y[i+1]
+    if cur>before and cur>after:
         count+=1
         
 print(count)
