8364d176f29add9b6a33366cf9a7837d027de52d
yoksamutr <164991929+yoksamutr@users.noreply.github.com>
2024-08-22 15:11:14 +0700

    Update 05_List_31.py

--- 05/05_List_31.py
+++ 05/05_List_31.py
@@ -4,28 +4,17 @@ ip=[str(e) for e in input().split()]
 order=str(input())
 
 def cut(l1):
-    front=[]
-    back=[]
-    for i, it1 in enumerate(l1):
-        if (i+1)*2<=len(l1):
-            back.append(it1)
-        else:
-            front.append(it1)
-    return front+back
+    n=len(l1)
+    return l1[n//2:]+l1[:n//2]
 
 def shuffle(l2):
-    front=[]
-    back=[]
-    for j, it2 in enumerate(l2):
-        if (j+1)*2<=len(l2):
-            front.append(it2)
-        else:
-            back.append(it2)
-         
-    new=[]   
-    for k, it in enumerate(front):
-        new.append(it)
-        new.append(back[k])
+    n=len(l2)
+    front=l2[:n//2]
+    back=l2[n//2:]
+    new=[]
+    for i in range(n//2):
+        new.append(front[i])
+        new.append(back[i])
     return new
 
 for ch in order:
