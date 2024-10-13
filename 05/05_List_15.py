de1f6e7577dfa4a793f487ac318395fd9630cf75
yoksamutr <164991929+yoksamutr@users.noreply.github.com>
2024-10-13 16:05:03 +0700

    Update 05_List_15.py

--- 05/05_List_15.py
+++ 05/05_List_15.py
@@ -1,9 +1,5 @@
-"""unique count"""
+"""Unique count"""
 
-lt=[int(e) for e in input().split()]
-
-s=set(lt)    
-print(len(s))
-
-ans=list(s)[:10]
-print(ans)
+st=set([int(e) for e in input().split()])
+print(len(st))
+print(list(st)[:10])
