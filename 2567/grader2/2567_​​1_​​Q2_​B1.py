4b67a686d93fcb5d307b5b1cb8f01651ef4db144
yoksamutr <164991929+yoksamutr@users.noreply.github.com>
2024-10-18 11:14:18 +0700

    Update 2567_​​1_​​Q2_​B1.py

--- 2567/grader2/2567_​​1_​​Q2_​B1.py
+++ 2567/grader2/2567_​​1_​​Q2_​B1.py
@@ -1,12 +1,11 @@
 """2567_1_Quiz_2_1"""
 
 D=[int(e) for e in input().split()]
-n=len(tuple(D))
 P=sorted(D)
 
 res=[]
 cnt=0
-for i in range(n):
+for i in range(len(D)):
     cnt+=P[i]
     idx=cnt%len(D)
     num=D.pop(idx)
