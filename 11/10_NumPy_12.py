27a033cdaea8da4f499a3d0cda563a46d0f32732
yoksamutr <164991929+yoksamutr@users.noreply.github.com>
2024-11-19 20:26:08 +0700

    Update 10_NumPy_12.py

--- 11/10_NumPy_12.py
+++ 11/10_NumPy_12.py
@@ -6,13 +6,11 @@ def toCelsius(f):
     return (f-32)*5/9
 
 def BMI(wh):
-    x=np.arange(wh.shape[0])
-    return wh[x,0]/(wh[x,1]/100)**2
+    return wh[:,0]/(wh[:,1]/100)**2
 
 def distanceTo(p,points):
-    x=np.arange(points.shape[0])
-    p=np.array(p)
-    arr=points[x]-p
-    return (arr[x,0]**2+arr[x,1]**2)**0.5
+    dx=p[0]-points[:,0]
+    dy=p[1]-points[:,1]
+    return (dx**2+dy**2)**0.5
 
 exec(input().strip())
