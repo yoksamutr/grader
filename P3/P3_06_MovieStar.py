d1538706342666a519db643e622b9f4a396b448b
yoksamutr <164991929+yoksamutr@users.noreply.github.com>
2024-11-27 22:47:46 +0700

    Update P3_06_MovieStar.py

--- P3/P3_06_MovieStar.py
+++ P3/P3_06_MovieStar.py
@@ -5,14 +5,12 @@ n=int(input())
 mp={}
 for _ in range(n):
     movie,n1,n2=input().split(", ")
-    if n1 in mp:
-        mp[n1].append(movie)
-    if n2 in mp:
-        mp[n2].append(movie)
     if n1 not in mp:
-        mp[n1]=[movie]
+        mp[n1]=[]
     if n2 not in mp:
-        mp[n2]=[movie]
+        mp[n2]=[]
+    mp[n1].append(movie)
+    mp[n2].append(movie)
         
 lt=input().split(", ")
 for name in lt:
